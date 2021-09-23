2b833034c3c12dca0d3d56f9a9e6d4af0ca1d0ae
brume7 <brumeakpadaka@gmail.com>
2021-09-23 13:50:46 +0200

    finalizing login page

--- app/src/main/java/com/example/nutrites/SignupTapFragment.java
+++ app/src/main/java/com/example/nutrites/SignupTapFragment.java
@@ -1,6 +1,7 @@
 package com.example.nutrites;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +15,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupTapFragment extends Fragment {
 
-    EditText email, pass, number, username;
+    EditText remail, rpass, rnumber, rusername;
     Button signup;
     float v=0;
 
@@ -26,10 +27,10 @@ public class SignupTapFragment extends Fragment {
 
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.signup_tap_fragment, container, false);
 
-        email = root.findViewById(R.id.semail);
-        number = root.findViewById(R.id.snumber);
-        pass = root.findViewById(R.id.spass);
-        username = root.findViewById(R.id.susername);
+        remail = root.findViewById(R.id.semail);
+        rnumber = root.findViewById(R.id.snumber);
+        rpass = root.findViewById(R.id.spass);
+        rusername = root.findViewById(R.id.susername);
         signup = root.findViewById(R.id.signupbutton);
 
         signup.setOnClickListener(new View.OnClickListener() {
@@ -38,16 +39,57 @@ public class SignupTapFragment extends Fragment {
                 rootNode = FirebaseDatabase.getInstance();
                 reference = rootNode.getReference("users");
 
-                String email = SignupTapFragment.this.email.getText().toString();
-                String number = SignupTapFragment.this.number.getText().toString();
-                String username = SignupTapFragment.this.username.getText().toString();
-                String pass = SignupTapFragment.this.pass.getText().toString();
+                String email = SignupTapFragment.this.remail.getText().toString();
+                String number = SignupTapFragment.this.rnumber.getText().toString();
+                String username = SignupTapFragment.this.rusername.getText().toString();
+                String pass = SignupTapFragment.this.rpass.getText().toString();
+                String noWhitespace = "\\A\\w{4,20}\\z";
 
-                UserHelperClass userHelperClass = new UserHelperClass(email,number,username,pass);
+                if (email.isEmpty()) {
+                    remail.setError("Field cannot be empty");
+
+                } else {
+                    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                        remail.setError("Input Valid Email");
+
+                    }else {
+                        if (number.isEmpty()) {
+                            rnumber.setError("Field cannot be empty");
+
+                        } else {
+                            if (!Patterns.PHONE.matcher(number).matches()){
+                                rnumber.setError("Input Valid Number");
+
+                            }else {
+
+                            if (username.isEmpty()) {
+                                rusername.setError("Field cannot be empty");
+
+                            } else {
+
+                                if (!username.matches(noWhitespace)) {
+                                    rusername.setError("White Spaces are not allowed");
+
+                                } else {
+
+                                if (pass.isEmpty()) {
+                                    rpass.setError("Field cannot be empty");
+
+                                } else {
+
+                                    UserHelperClass userHelperClass = new UserHelperClass(email, number, username, pass);
 
 
+                                    reference.child(username).setValue(userHelperClass);
+                                }
+                            }
 
-                reference.child(username).setValue(userHelperClass);
+
+                            }
+                        }
+                        }
+                    }
+            }
             }
         });
 
