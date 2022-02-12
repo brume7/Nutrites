b997574cb821b5b64f87cc92d3b376497ca38ccd
Oghenebrume <brumeakpadaka@gmail.com>
2022-02-12 16:55:06 +0100

    Meal generator code

--- app/src/main/java/com/example/nutrites/MealPlansTabFragment.java
+++ app/src/main/java/com/example/nutrites/MealPlansTabFragment.java
@@ -4,14 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 public class MealPlansTabFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.mealplans_tab_fragment, container, false);
+        ViewPager breakfastviewPager;
+        ViewPager lunchviewPager;
+//
+        BViewPagerAdapter bviewPagerAdapter;
+        breakfastviewPager = (ViewPager) root.findViewById(R.id.breakfastviewPager);
+        bviewPagerAdapter = new BViewPagerAdapter(getContext().getApplicationContext());
+        breakfastviewPager.setAdapter(bviewPagerAdapter);
+
+        LViewPagerAdapter lviewPagerAdapter;
+        lunchviewPager = (ViewPager) root.findViewById(R.id.lunchviewPager);
+        lviewPagerAdapter = new LViewPagerAdapter(getContext().getApplicationContext());
+        lunchviewPager.setAdapter(lviewPagerAdapter);
+
         return root;
     }
 }
