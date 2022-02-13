6c1f91524808e3123f9302b47c91b57fd563f295
Oghenebrume <brumeakpadaka@gmail.com>
2022-02-13 11:08:34 +0100

    calorie calc almost working

--- app/src/main/java/com/example/nutrites/CaloriesTabFragment.java
+++ app/src/main/java/com/example/nutrites/CaloriesTabFragment.java
@@ -1,6 +1,7 @@
 package com.example.nutrites;
 
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,14 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.Authenticator;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.Credentials;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -21,46 +27,31 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.Route;
 
-public class CaloriesTabFragment extends Fragment{
+public class CaloriesTabFragment extends Fragment {
     String food;
     EditText et;
     Button calcBc;
-    TextView tv1, tv2,tv3, tv4, tv5;
+    TextView tv1, tv2, tv3, tv4, tv5;
     View v1, v2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.calories_tab_fragment, container, false);
-        et = (EditText)root.findViewById(R.id.editTextCalories);
-        tv1 = (TextView)root.findViewById(R.id.textView13);
-        tv2 = (TextView)root.findViewById(R.id.textView16);
-        tv3 = (TextView)root.findViewById(R.id.textView17);
-        tv4 = (TextView)root.findViewById(R.id.textView18);
-        tv5 = (TextView)root.findViewById(R.id.textView19);
-        calcBc = (Button)root.findViewById(R.id.calculateBtn) ;
-        food=et.getText().toString();
+        et = (EditText) root.findViewById(R.id.editTextCalories);
+        tv1 = (TextView) root.findViewById(R.id.textView13);
+        tv2 = (TextView) root.findViewById(R.id.textView16);
+        tv3 = (TextView) root.findViewById(R.id.textView17);
+        tv4 = (TextView) root.findViewById(R.id.textView18);
+        tv5 = (TextView) root.findViewById(R.id.textView19);
+        calcBc = (Button) root.findViewById(R.id.calculateBtn);
+        food = et.getText().toString();
 
 
         calcBc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                OkHttpClient client = new OkHttpClient();
-
-                Request request = new Request.Builder()
-                        .url("https://food-calorie-data-search.p.rapidapi.com/api/search?keyword="+food+"")
-                        .get()
-                        .addHeader("x-rapidapi-host", "food-calorie-data-search.p.rapidapi.com")
-                        .addHeader("x-rapidapi-key", "")
-                        .build();
-//                Response response = client.newCall(request).execute();
-
-                Response response;
-                try {
-                    tv1.setText(client.newCall(request).execute().toString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                fetchData(food);
 
             }
         });
@@ -68,6 +59,46 @@ public class CaloriesTabFragment extends Fragment{
 
         return root;
     }
+
+    private void fetchData(String food) {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://calorieninjas.p.rapidapi.com/v1/nutrition?query="+food+"")
+                .get()
+                .addHeader("x-rapidapi-host", "calorieninjas.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "577c954e1cmsh29073eee6626404p1275dbjsn8466a95f2476")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(getContext().getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+
+                    String resp = response.body().string();
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                JSONObject jsonObject = new JSONObject(resp);
+                                String val1 = jsonObject.getString("items");
+                                tv2.setText("energy kilo calories is "+ val1 );
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+
+                }
+            }
+        });
+    }
+
 
 
 }
